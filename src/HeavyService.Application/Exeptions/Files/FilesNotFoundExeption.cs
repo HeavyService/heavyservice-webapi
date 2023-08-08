67b88abdbb78e89d6d41e4604d40c51916eaa120
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-08 12:27:11 +0500

    File not found exeption

---
+++ src/HeavyService.Application/Exeptions/Files/FilesNotFoundExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Application.Exeptions.Files;
+
+public class FilesNotFoundExeption : NotFoundExeption
+{
+    public FilesNotFoundExeption()
+    {
+        this.TitleMessage = "File not found!";
+    }
+}
