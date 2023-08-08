78a23d9c8677e585fba5f2232bb1f496c00eb205
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-08 12:26:27 +0500

    Auth not found exeption

---
+++ src/HeavyService.Application/Exeptions/Auth/PasswordIncorrectException.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Application.Exeptions.Auth;
+
+public class PasswordIncorrectException : BadRequestExeption
+{
+    public PasswordIncorrectException()
+    {
+        TitleMessage = "Password is Invalid!";
+    }
+}
