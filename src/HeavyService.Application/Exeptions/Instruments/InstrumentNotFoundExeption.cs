beef8822db04ada97df4ab1339cf87fa821870ef
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-08 12:27:32 +0500

    Instrument not found exeption

---
+++ src/HeavyService.Application/Exeptions/Instruments/InstrumentNotFoundExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Application.Exeptions.Instruments;
+
+public class InstrumentNotFoundExeption : NotFoundExeption
+{
+    public InstrumentNotFoundExeption()
+    {
+        this.TitleMessage = "Instrument not found!";
+    }
+}
