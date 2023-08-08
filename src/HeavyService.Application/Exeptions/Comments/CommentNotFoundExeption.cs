98be107e1449c8a95013734eeccad958c9342a37
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-08 12:26:53 +0500

    Comment not found exeption

---
+++ src/HeavyService.Application/Exeptions/Comments/CommentNotFoundExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Application.Exeptions.Comments;
+
+public class CommentNotFoundExeption : NotFoundExeption
+{
+    public CommentNotFoundExeption()
+    {
+        this.TitleMessage = "Comment not found!";
+    }
+}
