0de023035aa0af6a18a244824643e9bb95ccfefb
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-08 12:28:21 +0500

    Users Exeptions

---
+++ src/HeavyService.Application/Exeptions/Users/UserCacheDateExpiredExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Application.Exeptions.Users;
+
+public class UserCacheDateExpiredExeption : ExpiredExeption
+{
+    public UserCacheDateExpiredExeption()
+    {
+        TitleMessage = "User data has expired!";
+    }
+}
---
+++ src/HeavyService.Application/Exeptions/Users/UserNotFoundExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Application.Exeptions.Users;
+
+public class UserNotFoundExeption : NotFoundExeption
+{
+    public UserNotFoundExeption()
+    {
+        this.TitleMessage = "User not found!";
+    }
+}
