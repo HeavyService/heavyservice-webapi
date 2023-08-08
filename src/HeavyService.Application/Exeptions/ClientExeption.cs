d9514123dc6662674755944d72227661658a62fe
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-08 12:29:25 +0500

    Created Exeptions

---
+++ src/HeavyService.Application/Exeptions/ClientExeption.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace HeavyService.Application.Exeptions;
+
+public abstract class ClientExeption : Exception
+{
+    public abstract HttpStatusCode StatusCode { get; }
+
+    public abstract string TitleMessage { get; protected set; }
+
+}
---
+++ src/HeavyService.Application/Exeptions/TooManyRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace HeavyService.Application.Exeptions;
+
+public class TooManyRequestException : ClientExeption
+{
+    public override HttpStatusCode StatusCode { get; } = HttpStatusCode.TooManyRequests;
+
+    public override string TitleMessage { get; protected set; } = String.Empty;
+}
