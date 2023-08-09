dccd47e8b04562bf64c58988a0a1c8ef87b41719
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-09 10:54:24 +0500

    transport repository create

---
+++ src/HeavyService.DataAccess/Repositories/Transports/TransportRepository.cs
@@ -0,0 +1,149 @@
+﻿using Dapper;
+using HeavyService.Application.Utils;
+using HeavyService.DataAccess.Interfaces.Transports;
+using HeavyService.DataAccess.ViewModels;
+using HeavyService.Domain.Entities.Transports;
+
+namespace HeavyService.DataAccess.Repositories.Transports;
+public class TransportRepository : BaseRepository, ITransportRepository
+{
+    public async Task<long> CountAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "select count(*) from transports";
+            var result = await _connection.QuerySingleAsync<long>(query);
+            return result;
+        }
+        catch
+        {
+
+            return 0;
+
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<int> CreateAsync(Transport entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "INSERT INTO public.transports(" +
+                "user_id, name, description, image_path, price_per_hours," +
+                " region, district, address, status, created_at, updated_at, phone_number)" +
+                "VALUES (@UserId, @Name, @Description, @ImagePath, @PricePerHours, @Region, @District," +
+                " @Address, @Status, @CreatedAt, @UpdatedAt, @PhoneNumber);";
+            var result = await _connection.ExecuteAsync(query, entity);
+            return result;
+        }
+        catch
+        {
+
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<int> DeleteAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = $"DELETE FROM transports WHERE id=@Id";
+            var result = await _connection.ExecuteAsync(query, new { Id = id });
+            return result;
+
+        }
+        catch
+        {
+
+            return 0;
+
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<IList<TransportViewModel>> GetAllAsync(Paginationparams @params)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = $"SELECT * FROM transports order by id desc " +
+                $"offset {0} limit {@params.PageSize}";
+            var result = (await _connection.QueryAsync<TransportViewModel>(query)).ToList();
+            return result;
+        }
+        catch
+        {
+
+            return new List<TransportViewModel>();
+
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<TransportViewModel> GetByIdAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "SELECT * FROM transports where id = @Id";
+            var result = await _connection.QuerySingleAsync<TransportViewModel>(query, new { Id = id });
+            return result;
+        }
+        catch
+        {
+
+            return null;
+
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public Task<(int ItemsCount, IList<TransportViewModel>)> SearchAsync(string search, Paginationparams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<int> UpdateAsync(long id, Transport entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "UPDATE public.transports" +
+                "SET user_id=@UserId, name= @Name, description=@Description, " +
+                "image_path= @ImagePath, price_per_hours=@PricePerHours, region=@Region," +
+                " district=@District, address=@Address, status= @Status, created_at=@CreatedAt," +
+                " updated_at=@UpdatedAt, phone_number=@PhoneNumber" +
+                $"WHERE id = {id};";
+            var result = await _connection.ExecuteAsync(query, entity);
+            return result;
+        }
+        catch
+        {
+
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+}
