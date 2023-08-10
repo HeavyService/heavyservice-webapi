a85d101ff1c16577ff91f0bc710a19e87d85b59b
AkmalovAziz <akmalov844@gmail.com>
2023-08-10 10:27:40 +0500

    Create InstrumentValidator and AuthDto

---
+++ src/HeavyService.Persistance/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HeavyService.Persistance.Dtos.Auth;
+
+public class RegisterDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- src/HeavyService.Persistance/Dtos/Instruments/InstrumentCreateDto.cs
+++ src/HeavyService.Persistance/Dtos/Instruments/InstrumentCreateDto.cs
@@ -1,12 +1,13 @@
 ﻿using HeavyService.Domain.Enums;
+using Microsoft.AspNetCore.Http;
 
-namespace HeavyService.Domain.Entities.Instruments;
+namespace HeavyService.Persistance.Dtos.Instruments;
 
-public class Instrument : AudiTable
+public class InstrumentCreateDto
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string ImagePath { get; set; } = string.Empty;
+    public IFormFile ImagePath { get; set; } = default!;
     public int PricePerDay { get; set; }
     public string Region { get; set; } = string.Empty;
     public string District { get; set; } = string.Empty;
