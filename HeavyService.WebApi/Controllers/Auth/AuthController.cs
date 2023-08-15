6d93b8ba6232da0131f633f9c6ce76abaa18ced7
DotnetUzDev <abduganiyev767@gmail.com>
2023-08-15 16:30:48 +0500

    Auth controller catolikni togirladim

--- HeavyService.WebApi/Controllers/Auth/AuthController.cs
+++ HeavyService.WebApi/Controllers/Auth/AuthController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HeavyService.WebApi.Controllers.Auth
 {
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
